63e47ada9ffc13858b0cd5ed7a394e08dea9228c
mahinur05 <mahi121.mr2@gmail.com>
2023-02-04 00:49:24 +0600

    updated

--- index.js
+++ index.js
@@ -110,7 +110,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("Hello World! ok");
 });
 
 app.listen(port, () => {
